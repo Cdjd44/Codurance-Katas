8ae87e90b7a78bea2d462a899f5e7faddd5e068e
Craig Doyle <40270328@live.napier.ac.uk>
2022-05-09 16:29:37 +0100

    added Is Not a Leap Year test

--- Kata2/Kata2_Tests/UnitTest1.cs
+++ Kata2/Kata2_Tests/UnitTest1.cs
@@ -7,6 +7,12 @@ namespace Kata2_Tests
     public class UnitTest1
     {
         [TestMethod]
+        public void Is_Not_Leap_Year()
+        {
+            Assert.AreEqual(false, new Year().IsLeap(2015));
+        }
+
+        [TestMethod]
         public void Is_Leap_Year()
         {
             Assert.AreEqual(true, new Year().IsLeap(2016));
