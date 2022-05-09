082e1c665e7f0761c67540cb200f15aa32629c4d
Craig Doyle <40270328@live.napier.ac.uk>
2022-05-09 16:13:11 +0100

    framework for first test

--- Kata2/Kata2_Tests/UnitTest1.cs
+++ Kata2/Kata2_Tests/UnitTest1.cs
@@ -6,8 +6,10 @@ namespace Kata2_Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void number_is_divisable_by_4()
         {
+            var year = new Year(2006);
+            Assert.AreEqual(year.IsLeap(), Is.True);
         }
     }
 }
