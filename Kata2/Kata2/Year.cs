cc60edd6082e8037684aee96354872167bf3c323
Craig Doyle <40270328@live.napier.ac.uk>
2022-05-09 16:37:40 +0100

    kata finished (needs refactored)

--- Kata2/Kata2/Year.cs
+++ Kata2/Kata2/Year.cs
@@ -10,6 +10,8 @@ namespace Kata2
 
         public bool IsLeap(int year)
         {
+            if(year % 100 == 0 && year % 400 > 0) { return false; }
+            if(year % 400 == 0) { return true; }
             if(year % 4 == 0) { return true; }
             return false;
         }
--- Kata2/Kata2_Tests/UnitTest1.cs
+++ Kata2/Kata2_Tests/UnitTest1.cs
@@ -9,13 +9,27 @@ namespace Kata2_Tests
         [TestMethod]
         public void Is_Not_Leap_Year()
         {
-            Assert.AreEqual(false, new Year().IsLeap(2015));
+            Assert.AreEqual(false, new Year().IsLeap(1997));
         }
 
         [TestMethod]
-        public void Is_Leap_Year()
+        public void Is_Not_Leap_Year_if_Divisable_by_100_but_not_400()
         {
-            Assert.AreEqual(true, new Year().IsLeap(2016));
+            Assert.AreEqual(false, new Year().IsLeap(1800));
         }
+
+        [TestMethod]
+        public void Is_Leap_Year_if_Divisable_by_4()
+        {
+            Assert.AreEqual(true, new Year().IsLeap(1996));
+        }
+
+        [TestMethod]
+        public void Is_Leap_Year_if_Divisable_by_400()
+        {
+            Assert.AreEqual(true, new Year().IsLeap(1600));
+        }
+
+        
     }
 }
